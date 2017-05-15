5fb859284960c5f7c997d35f84f181c715a8a325
s-wangty <s-wangty@bsd405.org>
2017-05-15 12:03:44 -0700

    z

--- src/z/Z.java
+++ src/z/Z.java
@@ -1,5 +1,13 @@
 package z;
 
 public class Z {
+	
+	public static void main(String[] args){
+		
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MAX_VALUE + 1);
+		System.out.println(Integer.MIN_VALUE);
+		
+	}
 
 }
